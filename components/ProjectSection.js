06b8035350cec86718f55ebcf4871f0cf64e9f08
DenzelMwangi <denzelmwangi11@gmail.com>
2026-06-06 15:44:30 +0300

    created a project section

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ProjectSection() {
   return (
     <div className=' border-t border-gray-400 mt-4 '>
-        
+        <h1>Projects </h1>
+        div.
     </div>
   )
 }
